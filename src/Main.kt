843f30b67f99eb7c9bc8a8dbc3b8d71e376a57d1
nikeshjs <nikeshjs123@gmail.com>
2026-04-29 11:57:02 +0545

    Day 0 of learning kotlin

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Hello World!");
+    val name:String = "Umubwebwebwe Unuthwemwebwe Umwebuwosas";
+    var age = 10;
+    println("Hey, $name! You are $age years old");
+
+    age = 11;
+    println("Age changed to $age");  //age declared as var
+}
+
+//Semicolon not needed, but I will keep it.
+
+/* Differences between var and val:
+The variables declared with val cannot be changed later. But, the value of var can be changed.*/
+
