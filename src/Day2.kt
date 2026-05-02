d67fce579034455f80b4561a9d847a7acd4847ac
nikeshjs <nikeshjs123@gmail.com>
2026-05-02 18:58:51 +0545

    hello

--- src/Day2.kt
+++ src/Day2.kt
@@ -15,20 +15,29 @@ fun main() {
     //Although both are different in reference, the array's values itself can be updated in both cases.
     array1[0] = 7;
     array2[0] = 9; //Index 0 of both arrays are updated with the new one.
+
+    //Datatypes in detail
+    var num1 = 1; //Implicitly declared as Int
+    var num2: Short = 1; //Explicit declaration
+    var num3: Long = 1;
+    var num4 = 1L; //Also declared like this
+    var num5 = 2.0; //Double as default
+    var num6 = 2.0F; //Declared as float
+
+    //Another important thing:
+    var num = 2; //Int
+    //var number: Long = num; This creates an error as number is Long and num is Int. Type mismatch
+    var number: Long = num.toLong(); //Makes sure both are of the same datatype and holds reference to the same value
+
+    //toByte(), toLong(), toShort(), toDouble(), toInt(), toFloat()
+
+
+
+
+
+
+
+
 }
 
-//Datatypes in detail
-var num1 = 1; //Implicitly declared as Int
-var num2: Short = 1; //Explicit declaration
-var num3: Long = 1;
-var num4 = 1L; //Also declared like this
-var num5 = 2.0; //Double as default
-var num6 = 2.0F; //Declared as float
-
-//Another important thing:
-var num = 2; //Int
-//var number: Long = num; This creates an error as number is Long and num is Int. Type mismatch
-var number: Long = num.toLong(); //Makes sure both are of the same datatype and holds reference to the same value
-
-//toByte(), toLong(), toShort(), toDouble(), toInt(), toFloat()
 
