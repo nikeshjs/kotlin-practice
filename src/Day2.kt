d7e6021e39e8868a5c8130153f14c0f8de7b3afa
nikeshjs <nikeshjs123@gmail.com>
2026-05-02 10:01:32 +0545

    Another commit

---
+++ src/Day2.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var x = 1;
+    while(x < 3){
+        print(if(x==1) "Yab" else "Dab");
+        print("ba");
+        x++;
+    }
+    print("Do");
+}
+
+//Datatypes in detail
+var num1 = 1; //Implicitly declared as Int
+var num2: Short = 1; //Explicit declaration
+var num3: Long = 1;
+var num4 = 1L; //Also declared like this
+var num5 = 2.0; //Double as default
+var num6 = 2.0F; //Declared as float
+
