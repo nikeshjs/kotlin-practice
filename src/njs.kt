3ff04debfd1a5c1055714eaa0fb8d442d9d68d10
nikeshjs <nikeshjs123@gmail.com>
2026-05-27 21:24:34 +0545

    back again

--- src/njs.kt
+++ src/njs.kt
@@ -3,4 +3,9 @@ fun main() {
     val redNumbers = listOf(17, 2)
     val allCount = greenNumbers.count() + redNumbers.count()
     println(allCount)
+
+    val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
+    val requested = "smtp"
+    val isSupported = requested.uppercase() in SUPPORTED// Write your code here
+    println("Support for $requested: $isSupported")
 }
